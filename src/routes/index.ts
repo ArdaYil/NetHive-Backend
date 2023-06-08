4f341c87bc3a73c9c0dab49f42f6b985a9918973
= <ardayildirim05@icloud.com>
2023-06-08 22:59:22 +0200

    Set up express server

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,11 @@
+import express from "express";
 
+const app = express();
+const defaultPort = 8000;
+const port = process.env.PORT || defaultPort;
+
+const server = app.listen(port, () =>
+  console.log(`Listening on port ${port}...`)
+);
+
+export default server;
